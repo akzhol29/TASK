c83266d050760cb66c54575fdb990f6ad1bf5ff5
akzhol29 <112474645+akzhol29@users.noreply.github.com>
2022-09-05 23:28:15 +0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.Clear();
+Console.Write("Введите цело число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int square = num * num;
+Console.WriteLine($"Квадрат числа {num} равен {square}");
